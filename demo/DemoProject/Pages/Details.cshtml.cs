54a61d103ba6d0955b2e46c884a4fb5ea5d8dc29
JP <jp@jp.nl>
2024-02-27 14:19:34 +0100

    Meer view-zaken laten zien

---
+++ demo/DemoProject/Pages/Details.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DemoProject.Pages;
+
+public class Details : PageModel
+{
+    public void OnGet(int? id)
+    {
+        Console.WriteLine("Details met id " + id);
+    }
+
+    public void OnGetRegister(string hoi)
+    {
+        Console.WriteLine("register " + hoi);
+    }
+
+    public void OnGetSubscribe()
+    {
+        Console.WriteLine("subscribe");
+    }
+}
