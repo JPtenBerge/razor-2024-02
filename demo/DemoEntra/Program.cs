09b1323f63ca107346c5fd2a5631a6581e05bfcb
JP <jp@jp.nl>
2024-03-01 13:20:01 +0100

    Auth deel 2 laten zien

---
+++ demo/DemoEntra/Program.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"));
+
+builder.Services.AddAuthorization(options =>
+{
+    // By default, all incoming requests will be authorized according to the default policy.
+    options.FallbackPolicy = options.DefaultPolicy;
+    
+    options.AddPolicy("1337architecten", policy =>
+    {
+        policy.RequireAuthenticatedUser().RequireClaim("groups", "76ea7b84-df31-4012-9b40-857b79e42a0e");
+    });
+});
+builder.Services.AddRazorPages()
+    .AddMicrosoftIdentityUI();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+app.MapControllers();
+
+app.Run();
---
+++ users.md
@@ -0,0 +1,10 @@
+
+Redirect URL: https://localhost:7130
+
+Ruud:
+user: ruud@jptenbergelive.onmicrosoft.com
+pass: askmlasd98(*)DF(*)DSF()*_q
+
+Loser:
+user: loser@jptenbergelive.onmicrosoft.com
+pass: asdjkas)(*)(ASD())(*F(*)dasfdxcQWWAD
